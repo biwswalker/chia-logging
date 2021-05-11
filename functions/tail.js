ae7bd97cd1a0b27c14ae968000045e8b98e642fd
test <thanakonngam@gmail.com>
2021-05-11 16:53:14 +0700

    add log root location

--- functions/tail.js
+++ functions/tail.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const tailing_log = () => {
     const root_path = process.cwd().split(path.sep, 3).join('/')
-
+    console.log(`root: ${root_path}/.chia/mainnet/log/debug.log`)
     const tail = new Tail(`${root_path}/.chia/mainnet/log/debug.log`);
     tail.on("line", function (data) {
         console.log(data);
